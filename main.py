aa0701baf917b4d3c30df73806f85e3413885460
Yuto622 <125531107+Yuto622@users.noreply.github.com>
2023-06-05 18:55:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,31 @@
 import pandas as pd
 import random
 from enum import Enum
-#全種類のカードを生成するクラス
+from cards import Cards
+from typing import List, Dict
 
+#カード全種類を生成する関数
+def cards_options() -> list:
+    added_cards_options = []
+    for card in Cards:
+        added_cards = card.value
+        added_cards_options.append(added_cards)
+    return added_cards_options
 
-class Cards(Enum):
-  SPADE_2 = {"suit": "spade", "number": "2", "looks": "♠2"}
-  SPADE_3 = {"suit": "spade", "number": "3", "looks": "♠3"}
-  SPADE_4 = {"suit": "spade", "number": "4", "looks": "♠4"}
-  SPADE_5 = {"suit": "spade", "number": "5", "looks": "♠5"}
-  SPADE_6 = {"suit": "spade", "number": "6", "looks": "♠6"}
-  SPADE_7 = {"suit": "spade", "number": "7", "looks": "♠7"}
-  SPADE_8 = {"suit": "spade", "number": "8", "looks": "♠8"}
-  SPADE_9 = {"suit": "spade", "number": "9", "looks": "♠9"}
-  SPADE_10 = {"suit": "spade", "number": "10", "looks": "♠10"}
-  SPADE_J = {"suit": "spade", "number": "J", "looks": "♠J"}
-  SPADE_Q = {"suit": "spade", "number": "Q", "looks": "♠Q"}
-  SPADE_K = {"suit": "spade", "number": "K", "looks": "♠K"}
-  SPADE_A = {"suit": "spade", "number": "A", "looks": "♠A"}
-
-  HEART_2 = {"suit": "heart", "number": "2", "looks": "♥2"}
-  HEART_3 = {"suit": "heart", "number": "3", "looks": "♥3"}
-  HEART_4 = {"suit": "heart", "number": "4", "looks": "♥4"}
-  HEART_5 = {"suit": "heart", "number": "5", "looks": "♥5"}
-  HEART_6 = {"suit": "heart", "number": "6", "looks": "♥6"}
-  HEART_7 = {"suit": "heart", "number": "7", "looks": "♥7"}
-  HEART_8 = {"suit": "heart", "number": "8", "looks": "♥8"}
-  HEART_9 = {"suit": "heart", "number": "9", "looks": "♥9"}
-  HEART_10 = {"suit": "heart", "number": "10", "looks": "♥10"}
-  HEART_J = {"suit": "heart", "number": "J", "looks": "♥J"}
-  HEART_Q = {"suit": "heart", "number": "Q", "looks": "♥Q"}
-  HEART_K = {"suit": "heart", "number": "K", "looks": "♥K"}
-  HEART_A = {"suit": "heart", "number": "A", "looks": "♥A"}
-
-  DIAMOND_2 = {"suit": "diamond", "number": "2", "looks": "♦2"}
-  DIAMOND_3 = {"suit": "diamond", "number": "3", "looks": "♦3"}
-  DIAMOND_4 = {"suit": "diamond", "number": "4", "looks": "♦4"}
-  DIAMOND_5 = {"suit": "diamond", "number": "5", "looks": "♦5"}
-  DIAMOND_6 = {"suit": "diamond", "number": "6", "looks": "♦6"}
-  DIAMOND_7 = {"suit": "diamond", "number": "7", "looks": "♦7"}
-  DIAMOND_8 = {"suit": "diamond", "number": "8", "looks": "♦8"}
-  DIAMOND_9 = {"suit": "diamond", "number": "9", "looks": "♦9"}
-  DIAMOND_10 = {"suit": "diamond", "number": "10", "looks": "♦10"}
-  DIAMOND_J = {"suit": "diamond", "number": "J", "looks": "♦J"}
-  DIAMOND_Q = {"suit": "diamond", "number": "Q", "looks": "♦Q"}
-  DIAMOND_K = {"suit": "diamond", "number": "K", "looks": "♦K"}
-  DIAMOND_A = {"suit": "diamond", "number": "A", "looks": "♦A"}
-
-  CLUB_2 = {"suit": "club", "number": "2", "looks": "♣2"}
-  CLUB_3 = {"suit": "club", "number": "3", "looks": "♣3"}
-  CLUB_4 = {"suit": "club", "number": "4", "looks": "♣4"}
-  CLUB_5 = {"suit": "club", "number": "5", "looks": "♣5"}
-  CLUB_6 = {"suit": "club", "number": "6", "looks": "♣6"}
-  CLUB_7 = {"suit": "club", "number": "7", "looks": "♣7"}
-  CLUB_8 = {"suit": "club", "number": "8", "looks": "♣8"}
-  CLUB_9 = {"suit": "club", "number": "9", "looks": "♣9"}
-  CLUB_10 = {"suit": "club", "number": "10", "looks": "♣10"}
-  CLUB_J = {"suit": "club", "number": "J", "looks": "♣J"}
-  CLUB_Q = {"suit": "club", "number": "Q", "looks": "♣Q"}
-  CLUB_K = {"suit": "club", "number": "K", "looks": "♣K"}
-  CLUB_A = {"suit": "club", "number": "A", "looks": "♣A"}
+added_cards_options: List[str] = cards_options()
 
 #二人でプレーすると仮定してカードを配布する関数
-def distibute_card() -> tuple:
-  for card in Cards:
-    print(card.value)
+def distribute_card(added_cards_options) -> tuple:
+    player_one = []
+    player_two = []
+    while len(set(player_one)) < 5:
+        player_one.append(random.choice(added_cards_options)["looks"])
+    while len(set(player_two)) < 5:
+        player_two.append(random.choice(added_cards_options)["looks"])
+    return player_one, player_two
 
+[player_one, player_two] = distribute_card(added_cards_options)
 
-
-"""
-def distibute_card() -> tuple:
-  cards = make_cards()
-  player_one = []
-  player_two = []
-  for _ in range(5):
-    player_one.append(random.choice(cards))
-  
-  for _ in range(5):
-    player_two.append(random.choice(cards))
-  return player_one,player_two
-
-[player_one,player_two] = distibute_card()
-"""
 
 
 #プレイヤー１が何個のペアを持っているか判定する関数
@@ -261,10 +205,10 @@ def judge_loyal_stright_flush(player_one):
 #カードを交換する関数
 def change_cards_one(player_one):
   how_many_card_change = int(input("何枚のカードを交換したいですか"))
-  cards = make_cards()
+  added_cards_options = cards_options()
   for _ in range(how_many_card_change):
     location_card= int(input("何番目のカードを交換したいですか"))
-    player_one[location_card - 1] = random.choice(cards)
+    player_one[location_card - 1] = random.choice(added_cards_options)["looks"]
   return print(player_one)
 
 #宣言する関数
@@ -278,10 +222,10 @@ def confident():
 #カードを交換する関数
 def change_cards_two(player_two):
   how_many_card_change = int(input("何枚のカードを交換したいですか"))
-  cards = make_cards()
+  added_cards_options = cards_options()
   for _ in range(how_many_card_change):
     location_card= int(input("何番目のカードを交換したいですか"))
-    player_two[location_card - 1] = random.choice(cards)
+    player_two[location_card - 1] = random.choice(added_cards_options)["looks"]
   return print(player_two)
 
 #どっちが勝ったか判定する関数
@@ -296,8 +240,8 @@ def which_win(situation_one, situation_two) -> str:
 
 
 def main():
-  cards = make_cards() 
-  [player_one,player_two] = distibute_card()
+  added_cards_options = cards_options()
+  [player_one,player_two] =  distribute_card(added_cards_options)
   print(player_one,player_two)
 
 
